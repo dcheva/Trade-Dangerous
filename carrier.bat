6f4c454e17012201c2437b69aced13a61a12d6d3
dcheva <dmitry.cheva@gmail.com>
2021-02-13 11:44:48 +0200

    '!!!Не работает!!!'

--- carrier.bat
+++ carrier.bat
@@ -1,2 +1,3 @@
+@echo '!!!Не работает количестово: в строке Tritium 515 720 52 494 710M 0.43 читает 710М как количество, а не как время!!!'
 trade.py run --capacity 1000 --credits 1b --from %1 --direct --ly-per 500 --pad-size L --no-planet -vv
 trade.py market %1 --selling -vv
